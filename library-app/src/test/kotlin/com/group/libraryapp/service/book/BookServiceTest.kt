5d9f74bb2485d27cc04886810c5f73190e13e59d
who-hoo <who.ho3ov@gmail.com>
2022-12-06 23:59:29 +0900

    ♻️ Enum Class를 활용해 책의 분야 리팩토링

--- library-app/src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ library-app/src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -2,6 +2,7 @@ package com.group.libraryapp.service.book
 
 import com.group.libraryapp.domain.book.Book
 import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.book.BookType
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
@@ -36,7 +37,7 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책 저장이 정상 동작한다")
     fun saveBookTest() {
         // given
-        val request = BookRequest("A", "COMPUTER")
+        val request = BookRequest("A", BookType.COMPUTER)
 
         // when
         bookService.saveBook(request)
@@ -45,7 +46,7 @@ class BookServiceTest @Autowired constructor(
         val results = bookRepository.findAll()
         assertThat(results).hasSize(1)
         assertThat(results[0].name).isEqualTo("A")
-        assertThat(results[0].type).isEqualTo("COMPUTER")
+        assertThat(results[0].type).isEqualTo(BookType.COMPUTER)
     }
 
     @Test
