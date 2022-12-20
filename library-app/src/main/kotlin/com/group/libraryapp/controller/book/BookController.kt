222498376a77f92d87613712108dfb33c16bc0c4
who-hoo <who.ho3ov@gmail.com>
2022-12-21 00:05:24 +0900

    ✨ 분야별 책 개수 통계 조회 기능 개발

--- library-app/src/main/kotlin/com/group/libraryapp/controller/book/BookController.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/controller/book/BookController.kt
@@ -3,6 +3,7 @@ package com.group.libraryapp.controller.book
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.service.book.BookService
 import org.springframework.web.bind.annotation.*
 
@@ -29,5 +30,10 @@ class BookController(
     @GetMapping("book/loan")
     fun countLoanedBook(): Int {
         return bookService.countLoanedBook()
+    }
+
+    @GetMapping("book/stat")
+    fun getBookStatistics(): List<BookStatResponse> {
+        return bookService.getBookStatistics()
     }
 }
